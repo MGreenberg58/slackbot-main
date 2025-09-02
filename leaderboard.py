0daf4ad62a6597d75b7cfe2646a3ccfb100d7da1
Greenberg, Matthew Jacob <30955675+MGreenberg58@users.noreply.github.com>
2025-09-02 17:41:27 -0400

    changes plot color and framing

--- leaderboard.py
+++ leaderboard.py
@@ -47,10 +47,9 @@ def get_progress(leaderboard, users, weekly_goal=4, metric=None, start=None, end
 	cmap = mcolors.LinearSegmentedColormap.from_list(
         "progress_cmap", ["red", "yellow", "green"]
     )
-	color = cmap(min(progress / 1.0, 1.0))  
 
-	fig, ax = plt.subplots(figsize=(6, 1), dpi=200)
-	ax.barh([0], [progress_clamped], color=color, height=0.4)
+	fig, ax = plt.subplots(figsize=(6, 1), dpi=200, constrained_layouts=True)
+	ax.barh([0], [progress_clamped], color=cmap, height=0.4)
 	ax.barh([0], [1], color="lightgray", alpha=0.3, height=0.4)  
 
 	ax.set_xlim(0, 1)
