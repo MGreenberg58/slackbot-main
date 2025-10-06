0346c5c32ac867a41d70139c6609d32d705410bb
mgreenberg33 <30955675+MGreenberg58@users.noreply.github.com>
2025-10-06 14:39:08 -0400

    fixed datetime for test

--- testtime.py
+++ testtime.py
@@ -3,12 +3,12 @@ from zoneinfo import ZoneInfo
 
 TEAM_TZ = ZoneInfo("America/New_York")
 
-now = datetime.datetime.now()
+now = datetime.now()
 start_time = (now - datetime.timedelta(days=(now.weekday()))).replace(hour=0, minute=0, second=0, microsecond=0)
 end_time = start_time + datetime.timedelta(days=7) - datetime.timedelta(microseconds=1)
 print(start_time, end_time)
 
-now = datetime.datetime.now(TEAM_TZ)
+now = datetime.now(TEAM_TZ)
 start_time = (now - datetime.timedelta(days=(now.weekday()))).replace(hour=0, minute=0, second=0, microsecond=0)
 end_time = start_time + datetime.timedelta(days=7) - datetime.timedelta(microseconds=1)
 print(start_time, end_time)
